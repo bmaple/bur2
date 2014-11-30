93014d9d762ecd24abee3f99342aa2044b74333d
benjamin maple <rationalwasp@gmail.com>
2014-11-30 16:50:04 -0500

    beginning login stuff added

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+require_once(user.php);
+if(isset($_POST['submit'])){
+    if($user->login()){
+        $user->RedirectToURL();
+    }
+}
+?>
+<html>
+<head>
+<title> </title>
+<head>
+<body>
+<form id ='login' action='login.php' method='post' accept-charset='UTF-8'>
+<input type='text' name='username' id='username' maxlength='20' />
+<input type='password' name='password' id='password' maxlength='20' />
+<input type='submit' name='submit' id='submit' />
+</form>
+</body>
+</html>
