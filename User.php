0c2e1c8137d3871553352415d859baee926e5c25
benjamin maple <rationalwasp@gmail.com>
2014-11-30 18:07:55 -0500

    more progress

--- User.php
+++ User.php
@@ -3,22 +3,27 @@ class User {
     var $username;
     var $password;
     var $dbConnection;
-    var $mysql = array(  'host' => 'bur.ccg2fbosv7le.us-west-2.rds.amazonaws.com',
+    var $randSess = "aer23fwef";
+    var $errors = array();
+    var $mysql = array(  
+        'host' => 'bur.ccg2fbosv7le.us-west-2.rds.amazonaws.com',
         'port' => '3306',
         'database' => 'Bureaucrat',
         'username' => 'bmaple',
-        'password' => 'security',);
+        'password' => 'security');
     function login(){
-        if (!empty($_POST['username']))
+        if (empty($_POST['username']))
         {
-            $this->HandleError("Please enter a username");
+            //$this->HandleError("Please enter a username");
+            return false;
         }
-        if (!empty($_POST['password']))
+        if (empty($_POST['password']))
         {
-            $this->HandleError("Please enter a password");
+            //$this->HandleError("Please enter a password");
+            return false;
         }
-        $this->$username = $_POST['username'];
-        $this->$password = $_POST['password'];
+        $this->username = $_POST['username'];
+        $this->password = $_POST['password'];
         if(!$this->auth())
         {
             return false;
@@ -27,20 +32,27 @@ class User {
         $_SESSION[$this->GetLoginSessionVar()] = $this->$username;
         return true;
     }
+    //function HandleError($error) {
+        //$this->error_message    
+    //}
+    function GetLoginSessionVar()
+    {
+    return md5($this->$randSess);
+    }
     function auth(){
-            $this->$dbConnection= new mysqli(
-            $this->$mysql['host'], 
-            $this->$mysql['username'], 
-            $this->$mysql['password'], 
-            $this->$mysql['database'], 
-            $this->$mysql['port']);                      
-        if($this->$dbConnection->connect_errno){
+            $this->dbConnection= new mysqli(
+            $this->mysql['host'], 
+            $this->mysql['username'], 
+            $this->mysql['password'], 
+            $this->mysql['database'], 
+            $this->mysql['port']);                      
+        if($this->dbConnection->connect_errno){
             return false;
         }
-        $auth_query="select password from users where username=$this->$username";
+        $auth_query="select password from users where username=$this->username";
             $result = mysqli_query($auth_query);
         mysqli_close($dbConnection); 
-        if($this->$password == $result)
+        if($this->password == $result)
             return true;
         return false;
     }
--- login.php
+++ login.php
@@ -10,10 +10,12 @@ if(isset($_POST['submit'])){
 <html>
 <head>
 <title> </title>
-<head>
+</head>
 <body>
 <form id ='login' action='login.php' method='post' accept-charset='UTF-8'>
+<label for="username">Username: </label>
 <input type='text' name='username' id='username' maxlength='20' /> <br />
+<label for="password">Password: </label>
 <input type='password' name='password' id='password' maxlength='20' /> <br />
 <input type='submit' name='submit' id='submit' />
 </form>
