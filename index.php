cae73fa097ec27a707b38e4008768e233ca1a9cb
benjamin <bmaple@umich.edu>
2014-12-04 02:54:48 -0500

    fixed session error

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(isset($_GET['logout']))
 <body>
 <h1>Hello <?php echo $user->username ?>, Just fixing the url cause I'm care too much</H1>
 <a style="font-size:34; float:right" href="index.php?logout=true">Logout</a><br/>
-<iframe width="1280" height="720" src="https://www.youtube.com/watch?v=W1Vm7-PXttk&autoplay=1" frameborder="0" allowfullscreen></iframe>
+<iframe width="1280" height="720" src="https://www.youtube.com/watch?v=dh6RB1RT9i0" frameborder="0" allowfullscreen></iframe>
 
 </body>
 </html>
